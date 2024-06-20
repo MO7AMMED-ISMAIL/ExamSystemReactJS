c20a9cf891fdc7efa10516e8fb84b7c73ecee5e7
Mona-Ali-Mostafa98 <monaali980628@gmail.com>
2024-06-20 22:44:20 +0300

    Fix: Rename Logged in Component from Lgoin.jsx to Login.jsx

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,30 +1,28 @@
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
-import Lgoin from "./components/AuthComponent/Lgoin";
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import Login from "./components/AuthComponent/Login";
 import HomeComponent from "./components/HomeComponent";
 import PrivateRoute from "./components/PrivateRoute";
 import AllStudent from "./components/Student/AllStudent";
 import UserProfile from "./components/AuthComponent/UserProfile";
 
-
-
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route path="/login" element={<Lgoin/>}/>
-        <Route path="/" element={<PrivateRoute><HomeComponent/></PrivateRoute>}/>
-        <Route path="/home" element={<PrivateRoute><HomeComponent/></PrivateRoute>}>
-          <Route path="profile" element={<UserProfile/>}/>
-          <Route path="student" element={<AllStudent/>}/>
-        </Route>
-      </>
-    )
-  );
-  return (
-    < >
-      <RouterProvider router={router} />
-    </>
-  );
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/" element={<PrivateRoute><HomeComponent/></PrivateRoute>}/>
+                <Route path="/home" element={<PrivateRoute><HomeComponent/></PrivateRoute>}>
+                    <Route path="profile" element={<UserProfile/>}/>
+                    <Route path="student" element={<AllStudent/>}/>
+                </Route>
+            </>
+        )
+    );
+    return (
+        < >
+            <RouterProvider router={router}/>
+        </>
+    );
 }
 
 export default App;

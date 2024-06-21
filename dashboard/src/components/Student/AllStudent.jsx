34ad9bb934bedd0bbfe4e7701baf9ac02c085589
Mona-Ali-Mostafa98 <monaali980628@gmail.com>
2024-06-22 00:59:53 +0300

    enhance: student list

--- dashboard/src/components/Student/AllStudent.jsx
+++ dashboard/src/components/Student/AllStudent.jsx
@@ -3,6 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { useDispatch, useSelector } from 'react-redux';
 import { allStudent } from '../../redux/studentSlice';
+import {Link} from "react-router-dom";
+import {Button, Modal, Table} from "react-bootstrap";
 
 function AllStudent() {
     const dispatch = useDispatch();
@@ -13,48 +15,89 @@ function AllStudent() {
     }, [dispatch]);
 
     return (
-        <div id="body_content" className="bg-light flex-fill mt-3">
-            <div className="p-2 d-md-none d-flex text-white bg-info">
-                <button className="navbar-toggler text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="sidebar">
-                <FontAwesomeIcon icon={faBars} />
-                </button>
-                <a href="!" className="navbar-brand ms-3">Cafeteria</a>
-                <hr />
-            </div>
-            <div className="p-3">
-                {status === 'loading' && <div className="spinner-border text-primary" role="status"></div>}
-                {status === 'failed' &&  <p className='alert alert-danger text-center mt-2'>{error}</p>}
-                <table className="table table-bordered table-hover table-responsive">
-                    <thead className="table-light">
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Age</th>
-                            <th>Image</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            students.map((student,index) => {
-
-                                if (student.role !== 'admin') {
-                                    return (
-                                        <tr key={student._id}>
-                                            <td>{index + 1}</td>
-                                            <td>{student.name}</td>
-                                            <td>{student.email}</td>
-                                            <td>{student.age}</td>
-                                            <td><img className='rounded-circle' src={student.image} alt="" style={{width: '50px', height: '50px'}}/></td>
+        <div className="mx- mt-5">
+            <div className="row justify-content-center align-items-center">
+                <div className="col">
+                    <div className="card shadow mb-5">
+                        <div className="card-header py-3">
+                            <div className="row justify-content-between align-items-center">
+                                <h4 className="col text-muted">Our Students</h4>
+                                {/*<div className="col-2 text-center">
+                                    <Link to="/students/0/edit" className="btn btn-outline-primary">
+                                        Add New Student
+                                    </Link>
+                                </div>*/}
+                            </div>
+                        </div>
+                        <div className="card-body">
+                            {status === "loading" && <div>Loading...</div>}
+                            {status === "failed" && <div>Error: {error}</div>}
+                            {status === "succeeded" && (
+                                <>
+                                    <Table
+                                        className="table-bordered table-hover table-responsive"
+                                        striped
+                                        bordered
+                                        hover
+                                    >
+                                        <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Image</th>
+                                            <th>Name</th>
+                                            <th>Email</th>
+                                            {/*<th>Actions</th>*/}
                                         </tr>
-                                    );
-                                } else {
-                                    return null;
-                                }
-                            })
-                        }
-                    </tbody>
-                </table>
+                                        </thead>
+                                        <tbody>
+                                        {students.map((student, index) => (
+                                            <tr key={student._id}>
+                                                <td>{index + 1}</td>
+                                                <td><img className='rounded-circle' src={student.image} alt=""
+                                                         style={{width: '50px', height: '50px'}}/></td>
+                                                <td>{student.name}</td>
+                                                <td>{student.email}</td>
+                                                {/*<td className="w-25">
+                                                    <div className="row justify-content-start align-items-start">
+                                                        <div className="col-auto">
+                                                            <Link
+                                                                to={`/students/${student._id}`}
+                                                                className="btn btn-outline-success p-2"
+                                                            >
+                                                                Show
+                                                            </Link>
+                                                        </div>
+
+                                                        <div className="col-auto">
+                                                            <Link
+                                                                to={`/students/${student._id}/edit`}
+                                                                className="btn btn-outline-warning p-2"
+                                                            >
+                                                                Edit
+                                                            </Link>
+                                                        </div>
+
+                                                        <div className="col-auto">
+                                                            <button
+                                                                type="button"
+                                                                className="btn btn-outline-danger p-2"
+                                                                onClick={() => handleShowConfirmation(student._id)}
+                                                            >
+                                                                Delete
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </td>
+*/}
+                                            </tr>
+                                        ))}
+                                        </tbody>
+                                    </Table>
+                                </>
+                            )}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );

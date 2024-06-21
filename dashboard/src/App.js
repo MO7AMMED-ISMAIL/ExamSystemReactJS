fbe180fe6d3111500e468335cbb962326813e68c
Mona-Ali-Mostafa98 <monaali980628@gmail.com>
2024-06-21 08:01:02 +0300

    step 5 : Add routs to App.js

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,20 +1,34 @@
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
 import Login from "./components/AuthComponent/Login";
-import HomeComponent from "./components/HomeComponent";
+import SharedLayout from "./layouts/SharedLayout";
 import PrivateRoute from "./components/PrivateRoute";
 import AllStudent from "./components/Student/AllStudent";
 import UserProfile from "./components/AuthComponent/UserProfile";
+import {ExamForm} from "./components/Exam/ExamForm";
+import {ExamList} from "./components/Exam/ExamList";
+import React from "react";
+import {ExamDetails} from "./components/Exam/ExamDetails";
+import {NotFound} from "./layouts/errors/NotFound";
+import {Error} from "./layouts/errors/Error";
+import {examDetailsLoader, examFormLoader} from "./loaders/loaders";
 
 function App() {
     const router = createBrowserRouter(
         createRoutesFromElements(
             <>
                 <Route path="/login" element={<Login/>}/>
-                <Route path="/" element={<PrivateRoute><HomeComponent/></PrivateRoute>}/>
-                <Route path="/home" element={<PrivateRoute><HomeComponent/></PrivateRoute>}>
+                <Route path="/" element={<PrivateRoute errorElement={<Error />}>
+                    <SharedLayout />
+                </PrivateRoute>
+                }>
                     <Route path="profile" element={<UserProfile/>}/>
-                    <Route path="student" element={<AllStudent/>}/>
+                    <Route path="students" element={<AllStudent/>}/>
+                    <Route path="exams" element={<ExamList/>} />
+                    <Route path="exams/new" element={<ExamForm/>} loader={examFormLoader} />
+                    <Route path="exams/:id" element={<ExamDetails/>} loader={examDetailsLoader} errorElement={<Error />}/>
+                    <Route path="exams/:id/edit" element={<ExamForm/>} errorElement={<Error />}/>
                 </Route>
+                <Route path='*' element={<NotFound/>}/>
             </>
         )
     );

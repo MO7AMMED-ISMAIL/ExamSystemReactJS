36d3ed590b2e8028bbb9d213c4a989fa271b85c0
Mona-Ali-Mostafa98 <monaali980628@gmail.com>
2024-06-21 07:53:24 +0300

    step 4 : use examSlice.js in store.js

--- dashboard/src/redux/store.js
+++ dashboard/src/redux/store.js
@@ -1,14 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {configureStore} from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
 import studentSlice from "./studentSlice";
+import examReducer from "./examSlice";
 
 
 const store = configureStore({
     reducer: {
         auth: authSlice,
-        students: studentSlice
+        students: studentSlice,
+        exams: examReducer,
     },
 })
-
 
 export default store; 

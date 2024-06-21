dc41926610f989a9b01eada4a370a54b967e6f70
Mona-Ali-Mostafa98 <monaali980628@gmail.com>
2024-06-21 07:09:41 +0300

    fix: error from deleted file

--- dashboard/src/layouts/SharedLayout.jsx
+++ dashboard/src/layouts/SharedLayout.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import Sidebar from '../components/Sidebar'
 import Navbar from './Navbar'
-// import Content from './Content'
 import { Outlet } from 'react-router-dom'
 
 function SharedLayout() {
